963afad310ea8f1591e19289300c14b8780feaae
dil-amerk <amerk@diligent.com>
2023-04-02 18:18:50 +0200

    Add Routing

--- todo/src/main.tsx
+++ todo/src/main.tsx
@@ -4,11 +4,24 @@ import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./store/store";
 import "./Translation/config";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import TodoList from "./pages/TodoList";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/list",
+    element: <TodoList />,
+  },
+]);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
